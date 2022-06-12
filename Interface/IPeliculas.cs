b70954542b05a8d9a6eeb6b9480e5cc09dfe6fbb
delko <pa-sanchez@hotmail.com>
2022-06-11 20:13:42 -0500

    Creacion de interface

---
+++ Interface/IPeliculas.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pruebaVSCodeAPI.Controllers;
+using pruebaVSCodeAPI.Models;
+
+namespace pruebaVSCodeAPI.Interface {
+    public interface IPeliculas {
+
+        Task<ActionResult<IEnumerable<Pelicula>>> GetPeliculas();
+
+        Task<ActionResult<Pelicula>> GetPelicula(int id);
+
+        Task<bool> InsertPelicula(Pelicula pelicula);
+
+        Task<bool> UpdatePelicula(Pelicula pelicula);
+
+        Task<bool> DeletePelicula(int id);
+    }
+}
